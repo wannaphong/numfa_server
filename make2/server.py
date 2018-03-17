8fce31319ab28642837c6df79c8344ae32d107f3
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2018-03-17 18:14:23 +0700

    update data

--- make2/server.py
+++ make2/server.py
@@ -3,22 +3,10 @@ from flask import Flask, render_template,request, jsonify
 from run import get_chatbot
 
 app = Flask(__name__)
-#app.config['SECRET_KEY'] = 'secret!'
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-"""@app.route('/chat')
-def chat():
-    return render_template('chat.html')
-"""
-"""@app.route('/api/chat_message', methods=['POST'])
-def add_message():
-    content = request.json
-    data=get_chatbot(content['text'])
-    return jsonify({"text":data})
-"""
 
 @app.route('/api/chat_message')
 def get_bot_response():
