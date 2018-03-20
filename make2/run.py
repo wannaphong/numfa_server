2e47c5c47d3435766756ce97879df54be1c4385f
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2018-03-20 22:24:42 +0700

    save file

--- make2/run.py
+++ make2/run.py
@@ -7,10 +7,15 @@ chatbot = ChatBot(
     storage_adapter='chatterbot.storage.SQLStorageAdapter', # กำหนดการจัดเก็บ ในที่นี้เลือก chatterbot.storage.SQLStorageAdapter เก็บเป็น Sqllite
     database='fah2.sqlite3', # ที่ตั้งฐานข้อมูล
     statement_comparison_function=thai,
+    preprocessors=[
+        'chatterbot.preprocessors.clean_whitespace',
+        'chatterbot.preprocessors.unescape_html'
+    ],
+    filters=['chatterbot.filters.RepetitiveResponseFilter'],
     logic_adapters=[
         'chatterbot.logic.BestMatch',
         'timenow.TimeLogicAdapter',
-        'weather.WeatherLogicAdapter',
+        #'weather.WeatherLogicAdapter',
         {
             'import_path':'chatterbot.logic.LowConfidenceAdapter',
             'threshold':0.65,
